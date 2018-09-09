a0d06dec1bf6b45ae648db26384a23c494392c59
vikash verma <vikashverma@vikashs-MacBook-Air.local>
2018-09-09 13:30:05 +0530

    updated css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css'
-
+import './style/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
